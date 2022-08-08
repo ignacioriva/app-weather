bc074d93eedde91a825a96bea16844395c01a4ee
ignacioriva <84292438+ignacioriva@users.noreply.github.com>
2022-08-08 00:32:47 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,12 +5,16 @@ const navResponsive = document.getElementById('navResponsive');
 const btnSeach = document.getElementById('btnSeach');
 const btnLocation = document.getElementById('btnLocation');
 
+
+
 //// FUNCTION SET DATE /////
 function setDate () {
 	const date = new Date();
 	const dateString = `Today ' ${date.toDateString().slice(0,-8)}, ${date.getDay()}`;
 	const dateInfo = document.getElementById('dateInfo').innerHTML = dateString;
-}setDate();
+}
+
+setDate();
 
 btnSeach.addEventListener('click', () => {
 	const inputSeach = document.getElementById('inputSeach');
@@ -27,7 +31,7 @@ btnSeach.addEventListener('click', () => {
 getData();
 
 });
-//// FUNCTION SET ALL VALUES WEATHER /////
+
 function setValuesWeather (obj) {
   const tempNumber = document.getElementById('tempNumber');
   const cityWheather = document.getElementById('cityWheather');
@@ -36,19 +40,23 @@ function setValuesWeather (obj) {
   const humidityPorcent = document.getElementById('humidityPorcent');
   const visibilityNumber = document.getElementById('visibilityNumber');
   const airPressureNumber = document.getElementById('airPressureNumber');
+  const iconMainWeather = document.getElementById('iconMainWeather');
 
   tempNumber.innerHTML = `${Math.round(obj.current.temp)}<span>°c</span>`;
   cityWheather.innerHTML = obj.timezone;
-  wheatherDescription.innerHTML = `${obj.current.weather[0].main}`;
+  wheatherDescription.innerHTML = `${obj.current.weather[0].description}`;
   windStatus.innerHTML = `${obj.current.wind_speed}mph`;
   humidityPorcent.innerHTML = `${obj.current.humidity}%`;
   visibilityNumber.innerHTML = `${obj.current.visibility}miles`;
   airPressureNumber.innerHTML = `${obj.current.pressure}mb`;
+  iconMainWeather.src = `https://openweathermap.org/img/wn/${obj.current.weather[0].icon}@4x.png`;
 
   const dailyDivMax = document.getElementsByClassName('weekTempMax');
   const dailyDivMin = document.getElementsByClassName('weekTempMin');
+  const imagesTemp = document.getElementsByClassName('weather-info');
 
   for (let i = 0; i < 5; i++){
+  	imagesTemp[i].src = `https://openweathermap.org/img/wn/${obj.daily[i].weather[0].icon}@4x.png`;
   	dailyDivMax[i].innerHTML = `${Math.round(obj.daily[i].temp.max)}°c`;
   	dailyDivMin[i].innerHTML = `${Math.round(obj.daily[i].temp.min)}°c`;
   }
@@ -69,7 +77,7 @@ btnLocation.addEventListener('click', () => {
   		console.log(obj);
 	});
 });
-///// API OPENCAGE ////
+
 const fetchData = (location) => {
   return new Promise((resolve, reject) => (
       fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=b41e80f9ba614931be5f0ee7170eeeac`)
@@ -78,7 +86,8 @@ const fetchData = (location) => {
         .catch(err => reject(err))
       ))
 };
-///// API OPENWEATHER ////
+
+
 const weatherAPI = (lat,long) => {
   return new Promise((resolve, reject) => (
       fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=bb53ccca89a7026be159aabf357646f9&units=metric`)
@@ -87,3 +96,13 @@ const weatherAPI = (lat,long) => {
         .catch(err => reject(err))
       ))
 };
+
+// async function init () {
+// 	const data = await fetchData("Buenos Aires");
+// 	  const lat = data.results[0].geometry.lat;
+// 	  const long = data.results[0].geometry.lng;
+// 	  const resp = await weatherAPI(lat,long);
+// 	  setValuesWeather(resp);
+// }
+
+// init();
